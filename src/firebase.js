304813a9d483f1ed34f3968b66338fb146bb317e
Akshay Wazir <waizaeer4@gmail.com>
2021-04-22 22:59:13 +0530

    another one

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyCz3xKLPc25lMP_d7To29S7rBHrFdw68Bg",
+  authDomain: "hutest-dev.firebaseapp.com",
+  projectId: "hutest-dev",
+  storageBucket: "hutest-dev.appspot.com",
+  messagingSenderId: "962193963474",
+  appId: "1:962193963474:web:0acaed1880fd480384f00b",
 });
 
 export const auth = app.auth();
